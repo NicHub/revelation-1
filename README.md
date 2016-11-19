1e9ec0f44edd65fba725f71ccc363261c526ab37
NicHub <nicolas.jeanmonod@gmail.com>
2016-11-19 23:41:57 +0100

    ateliers img->html + webhook

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 
+# OSONSNOUSREVELER.COM
+
 <http://osonsnousreveler.com/>
 
-<https://osonsnousreveler.github.io/revelation-1/>
+<https://www.facebook.com/Charlotte-Guignard-1195289173868280/?fref=ts>
+
+<https://developers.facebook.com/tools/debug/og/object/?q=http%3A%2F%2Fosonsnousreveler.com%2F>
---
+++ github-webhook.php
@@ -0,0 +1,25 @@
+<?php
+
+date_default_timezone_set( 'Europe/Paris' );
+$curDateTime = date( 'Y-m-d – H:i:s' );
+
+main();
+
+function main()
+{
+    global $curDateTime;
+    if( isset( $_POST[ 'payload' ] ) )
+    {
+        $myfile = fopen( "github-webhook.log", "w" ) or die ( "Impossible d’ouvrir “github-webhook.log” en écriture" );
+        fwrite( $myfile, "WEBHOOK\n" );
+        fwrite( $myfile, "Dernière mise à jour : " );
+        fwrite( $myfile, $curDateTime );
+        fclose( $myfile );
+
+        chdir( '$HOME/revelation-1/' );
+        $cmd = "git pull > /dev/null &";
+        exec( $cmd );
+    }
+}
+
+?>
