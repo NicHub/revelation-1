fa75c49ff201ff9af0f5876b1c5c11df909f7e3d
NicHub <nicolas.jeanmonod@gmail.com>
2016-12-30 08:49:32 +0100

    correction bug format date github-webhook.php

--- github-webhook.php
+++ github-webhook.php
@@ -1,24 +1,34 @@
 <?php
 
 date_default_timezone_set( 'Europe/Paris' );
-$curDateTime = date( 'Y-m-d<br />H:i:s' );
+$curDateTime = date( 'Y-m-d\<\b\r \/\>H:i:s' );
 
 main();
 
 function main()
 {
-    global $curDateTime;
     if( isset( $_POST[ 'payload' ] ) )
     {
-        $myfile = fopen( "github-webhook-log.json", "w" ) or die ( "Impossible d’ouvrir “github-webhook.log” en écriture" );
-        fwrite( $myfile, '{"github_webhook_last_pull_time":"' );
-        fwrite( $myfile, $curDateTime );
-        fwrite( $myfile, '"}' );
-        fclose( $myfile );
-
-        chdir( '$HOME/revelation-1/' );
-        $cmd = "git pull > /dev/null &";
-        exec( $cmd );
+        writeDateToJsonFile();
+        gitPull();
     }
 }
+
+function writeDateToJsonFile()
+{
+    global $curDateTime;
+    $myfile = fopen( "github-webhook-log.json", "w" ) or die ( "Impossible d’ouvrir “github-webhook.log” en écriture" );
+    fwrite( $myfile, '{"github_webhook_last_pull_time":"' );
+    fwrite( $myfile, $curDateTime );
+    fwrite( $myfile, '"}' );
+    fclose( $myfile );
+}
+
+function gitPull()
+{
+    chdir( '$HOME/revelation-1/' );
+    $cmd = "git pull > /dev/null &";
+    exec( $cmd );
+}
+
 ?>
